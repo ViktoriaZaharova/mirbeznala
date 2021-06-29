30867c7f92405523548e648a080c667d6cbe5004
Viktoria <zaharova181114@gmail.com>
2021-06-29 16:15:03 +0300

    add blocks

--- app/js/common.js
+++ app/js/common.js
@@ -1,27 +1,74 @@
-$(document).ready(function(){
+$(document).ready(function () {
     $(".new-clients-slider").owlCarousel({
         items: 4,
         nav: true,
         dots: false,
         margin: 30,
-        responsive : {
-            0 : {
-                items : 1,
+        loop: true,
+        responsive: {
+            0: {
+                items: 1,
 
             },
-            576 : {
-                items : 2,
+            576: {
+                items: 2,
             },
-            992 : {
-                items : 3,
+            992: {
+                items: 3,
                 margin: 0,
             },
-            1200 : {
-                items : 4,
+            1200: {
+                items: 4,
+            }
+        }
+    });
+
+    $('.staff-slider').owlCarousel({
+        items: 4,
+        nav: true,
+        dots: false,
+        margin: 30,
+        loop: true,
+        responsive: {
+            0: {
+                items: 1,
+                margin: 0,
+            },
+            576: {
+                items: 2,
+                margin: 20,
+            },
+            992: {
+                items: 3,
+            },
+            1200: {
+                items: 4,
+            }
+        }
+    });
+
+    $('.reviews-slider').owlCarousel({
+        items: 3,
+        nav: true,
+        dots: false,
+        margin: 30,
+        loop: true,
+        responsive: {
+            0: {
+                items: 1,
+                margin: 0,
+            },
+            768: {
+                items: 2,
+                margin: 20,
+            },
+            1200: {
+                items: 3,
             }
         }
     });
 });
+
 
 // accordeon
 function accordeon() {
@@ -37,3 +84,4 @@ function accordeon() {
 }
 
 accordeon();
+
